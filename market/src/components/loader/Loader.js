d99823d4ac911fb058ed7e045fe7cde2f350b031
Mushni-Chankseliani <71870911+Mushni-Chankseliani@users.noreply.github.com>
2021-07-09 23:22:58 +0400

    Update loader

--- market/src/components/loader/Loader.js
+++ market/src/components/loader/Loader.js
@@ -1,9 +1,9 @@
 import { TEST_IDS } from '../../utils/testids';
 
 export const Loader = ({ message = 'Loading...' }) => (
-  <div className="row">
-    <h2 className="text-center text-warning" data-testid={TEST_IDS.loader.message}>
-      {message}
-    </h2>
+  <div className="w-100 text-center">
+    <div className="spinner-border text-primary" role="status">
+      <span className="visually-hidden">{message}</span>
+    </div>
   </div>
 );
