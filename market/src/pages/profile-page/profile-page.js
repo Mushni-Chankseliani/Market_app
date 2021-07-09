1482768291aea5446b0715303ec4e471b0321202
Mushni-Chankseliani <71870911+Mushni-Chankseliani@users.noreply.github.com>
2021-07-09 23:11:49 +0400

    Feat(): update profile page

--- market/src/pages/profile-page/profile-page.js
+++ market/src/pages/profile-page/profile-page.js
@@ -1,9 +1,28 @@
+import { useSelector } from 'react-redux';
 import { withAuthProtected } from '../../hoc';
+import { currentUserSelector } from '../../redux/selectors';
 
 function ProfilePage() {
+  const user = useSelector(currentUserSelector);
   return (
-    <div>
-      <h1>this is Profile Page</h1>
+    <div className="row flex-column align-items-center flex-md-row justify-content-md-between">
+      <div className="col-10 col-xl-4 col-lg-6 col-md-6 mb-5">
+        <img src={user.avatar} alt={user.first_name} className="w-100" />
+      </div>
+      <div className="col-10 col-xl-6 col-lg-6 col-md-6 mx-auto">
+        <div className="mb-3 border-bottom pb-3">
+          <h3 className="mb-3">First Name:</h3>
+          <h4 className="text-muted">{user.first_name}</h4>
+        </div>
+        <div className="mb-3 border-bottom pb-3">
+          <h3 className="mb-3">Last Name:</h3>
+          <h4 className="text-muted">{user.last_name}</h4>
+        </div>
+        <div className="mb-3 border-bottom pb-3">
+          <h3 className="mb-3">Email:</h3>
+          <h4 className="text-muted">{user.email}</h4>
+        </div>
+      </div>
     </div>
   );
 }
